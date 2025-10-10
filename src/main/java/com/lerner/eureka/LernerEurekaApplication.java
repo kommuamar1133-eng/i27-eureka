a74f92b0904a28bc59666f6588b941534abdac2a
charyacomail <charyacomail@gmail.com>
2025-10-10 20:45:32 +0530

    added sonar stage

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -1,10 +1,17 @@
 package com.lerner.eureka;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+
 
 @SpringBootApplication
 @EnableEurekaServer
